bc5bc6615912894afd19c003a7f8035dad88b5bf
jasonstevens13 <jasonstevens13@yahoo.com>
2020-06-24 17:02:42 -0400

    edited workout model

--- models/Workout.js
+++ models/Workout.js
@@ -11,11 +11,20 @@ const transactionSchema = new Schema({
     exercises: [
         {
             type: String,
+            name: String,
+            duration: Number,
+            distance: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number,
             required: "Enter an exercise"
         }
 
     ]
 });
+
+
+
 
 const Transaction = mongoose.model("workouts", transactionSchema);
 
